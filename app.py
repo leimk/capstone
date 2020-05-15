199c38082414a36c11c513f5473cd1bf19f437ff
Michael Kusliawan <michaelkusliawan@gmail.com>
2020-05-15 21:16:49 +0700

    dengan 2 endpoint

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, request 
 import sqlite3
 import pandas as pd
-conn = sqlite3.connect("data/chinook.db")
-sqlStat_salesByMonth = "SELECT * FROM INVOICES order by invoiceDate"
-sales_df = pd.read_sql_query(sqlStat_salesByMonth, conn,parse_dates='InvoiceDate')
+# conn = sqlite3.connect("data/chinook.db")
+
 app = Flask(__name__) 
 
-@app.route('/sale/byMonth')
-def sales():
-	sales_df['bulan'] = sales_df['InvoiceDate'].dt.month_name()
-	salesByMonth = sales_df.groupby('bulan').sum()
-	salesbyMonthdf = pd.DataFrame(salesByMonth,columns=['Total'])
-	
-	return salesbyMonthdf.to_json()
+@app.route('/sale/by/<choice>')
+def sales(choice):
+	if(choice == 'bulan') :
+		conn = sqlite3.connect("data/chinook.db")
+		sqlStat_salesByMonth = "SELECT * FROM INVOICES order by invoiceDate"
+		sales_df = pd.read_sql_query(sqlStat_salesByMonth, conn,parse_dates='InvoiceDate')
+		sales_df['bulan'] = sales_df['InvoiceDate'].dt.month_name()
+		salesByMonth = sales_df.groupby('bulan').sum()
+		salesbyMonthdf = pd.DataFrame(salesByMonth,columns=['Total'])
+		
+		return salesbyMonthdf.to_json()
+	elif (choice == 'kuartal'):
+		conn = sqlite3.connect("data/chinook.db")
+		sqlStat_salesByQ = "SELECT * FROM INVOICES order by invoiceDate"
+		salesByQ = pd.read_sql_query(sqlStat_salesByQ,conn,parse_dates='InvoiceDate')
+		salesByQ['quarter']  = salesByQ['InvoiceDate'].dt.quarter
+		salesByQ = salesByQ.groupby('quarter').sum()
+		salesByQ_df = pd.DataFrame(salesByQ,columns=['Total'])
+		return salesByQ_df.to_json()
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
